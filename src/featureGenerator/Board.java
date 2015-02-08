4629632590346d28761df541acefafe4aa8422ba
Dan True <dbtrue@wpi.edu>
2015-02-08 18:42:33 -0500

    Takes out all unused Board code. Fixes checkMiddle function. Adds checkVReverse, which fixes the vertical connectN misscounts. Adds in a debug mode which will output a file with each board, and it's feature values. Debug mode will only run if variable debugMode (in DataReader class) is set to true.

--- src/featureGenerator/Board.java
+++ src/featureGenerator/Board.java
@@ -8,455 +8,126 @@ package featureGenerator;
 
 import java.io.FileWriter;
 import java.io.IOException;
-//import java.util.Random;
 
 public class Board {
 
 	int width;
 	int height;
 	int board[][];
-	int numOfDiscsInColumn[];
-	int emptyCell =0;
 	int N;
+	int emptyCell = 0;
 	int PLAYER1 = 1;
 	int PLAYER2 = 2;
-	int NOCONNECTION=-1;
-	int TIE = 0;
-	boolean player1Pop = false;
-	boolean player2Pop = false;
-	
 
+	/*
+	 * Constructor
+	 */
 	Board(int height, int width, int N)
 	{
 		this.N = N;
 		this.width = width;
 		this.height = height;
 		this.board = new int[height][width];
-		numOfDiscsInColumn=new int[this.width];
 	}
-	
+
 	/*
-	 * Takes in a correctly formatted String and returns a Board
+	 * Takes in a correctly formatted String and returns a Board.
 	 */
 	public static Board createBoardFromString(String boardStr) {
 		Board newBoard = new Board(6, 7, 4);
 		String[] boardArr = boardStr.split(",");
-		
+
 		for(int i = 0; i <= 6; i++) {
 			for(int j = 0; j <= 5; j++) {
 				newBoard.board[j][i] = Integer.parseInt(boardArr[6*i+5-j]);
 			}
 		}
-		
+
 		return newBoard;
 	}
-	
+
 	/*
 	 * Calculate the values for each feature (attribute)
 	 */
 	public String calculateFeatureValues() {
-		int featCheckMiddle = checkMiddle();
-		int featTwoInARow = checkNInARow(2);
-		int featThreeInARow = checkNInARow(3);
-		return featCheckMiddle + "," + featTwoInARow + "," + featThreeInARow + ",99,99";
-	}
-	
-	//Returns a new board with the same state as the given board
-	public Board clone()
-	{
-		int i = 0;
-		int j = 0;
-		Board b = new Board(this.height, this.width, this.N);
-		for(i = 0; i < b. height;i++)
-		{
-			for(j = 0; j < b.width; j++)
-			{
-				b.board[i][j] = this.board[i][j];
+
+		int feat1 = checkMiddle();
+		int feat2 = checkNInARow(2);
+		int feat3 = checkNInARow(3);
+		int feat4 = feat2 + 2*feat3;
+		int feat5 = 99;
+
+		String featuresStr = feat1 + 
+				"," + feat2 + "," + feat3 + 
+				"," + feat4 + "," + feat5;
+
+		if(DataReader.debugMode) {
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write("\nMiddle:  " + feat1 + "\n");
+				fw.write("2inARow: " + feat2 + "\n");
+				fw.write("3inARow: " + feat3 + "\n");
+				fw.write("Total:   " + feat4 + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 
-		for(int k = 0; k < this.width; k++)
-		{
-			b.numOfDiscsInColumn[k] = this.numOfDiscsInColumn[k];
-		}
-		b.player1Pop = this.player1Pop;
-		b.player2Pop = this.player2Pop;
-		return b;
+		return featuresStr;
 	}
 
-	//Determine a value for the given board
-	public double getHeuristic() {
-		double hval = 0;
-		for(int i = 2; i <= N; i++) {
-			hval += checkV(i) + checkH(i) + checkD1(i) + checkD2(i) + checkHReverse(i) + checkMiddle();
-		}
-		return hval;
-	}
-
+	/*
+	 * Returns the difference of the number of N in a rows between P1 and P2.
+	 */
 	public int checkNInARow(int numInARow) {
-		int Count = 0;
-		
-		Count+= checkV(numInARow)+ checkH(numInARow) + checkHReverse(numInARow) + checkD1(numInARow) + checkD2(numInARow);
-		return Count;
+
+		int count = checkVReverse(numInARow) + 
+				checkH(numInARow) + checkHReverse(numInARow) + 
+				checkD1(numInARow) + checkD2(numInARow);
+
+		return count;
 	}
-	
+
 	/*
 	 *  Gives a heuristic based on the desity of the chips.
 	 *  Returns a higher value if more chips in the center.
 	 *  Used as experiment, but decided not to use in heuristic calculation.
 	 */
 	public int checkMiddle() {
-		//int sign = -1;
-		int sum=0;
-		int weightCenter = 2;
-		int weightCenterOffset = 1;
+
 		int player1Count = 0;
 		int player2Count = 0;
-		
-//		for(int i = 0; i < this.width; i++) {
-//
-//			if(i >= (int)(width/2) -1 && i <= (int)(width/2) + 1) {
-//				sign = 1;
-//			}
-//			sum += sign * this.numOfDiscsInColumn[i];
-//		}
-		
-//		for(int i = 3; i <= 5; i++) {
-//			for(int j = 0; j < this.height; j++) {
-//				if(this.)
-//			}
-//		}
+		int[] weights = new int[]{0, 0, 1, 2, 1, 0, 0};
 
 		for(int i = 0; i < this.height; i++) {
-			if(this.board[3][i] == PLAYER1 || this.board[5][i] == PLAYER1) {
-				player1Count+= weightCenterOffset;
-			}
-			else if(this.board[3][i] == PLAYER2 || this.board[3][i] == PLAYER2) {
-				player2Count+= (-1) * weightCenterOffset;
-			}
-			
-			
-			if(this.board[4][i] == PLAYER1) {
-				player1Count+= weightCenter;
-			}
-			else if(this.board[4][i] == PLAYER2) {
-				player2Count+= (-1) *weightCenter;
-			}
-		}
-			
-		sum = player1Count + player2Count;
-
-		return sum;
-	}
-
-	//Prints the board to file
-	public void printBoard(){
-		try
-		{
-			String filename= "DebugBoard.txt";
-			FileWriter fw = new FileWriter(filename,true); //the true will append the new data
-			fw.write("Board \n");//appends the string to the file
-			for(int j = 0; j < this.width; j++)
-			{
-				fw.write(this.numOfDiscsInColumn[j] + " ");
-			}
-			for(int i=0;i<height;i++){
-				for(int j=0;j<width;j++){
-					fw.write(board[i][j]+" ");
+			for(int j = 0; j < this.width; j++) {
+				if(this.board[i][j] == PLAYER1) {
+					player1Count += weights[j];
 				}
-				fw.write("\n");
+				else if(this.board[i][j] == PLAYER2) {
+					player2Count += (-1) * weights[j];
+				}
 			}
-			fw.close();
 		}
-		catch(IOException ioe)
-		{
-			System.err.println("IOException: " + ioe.getMessage());
-		}
-
+		return (player1Count + player2Count);
 	}
-	
-	public String printBoard2() {
+
+	/*
+	 * Returns a String representation of the board.
+	 */
+	public String printBoard() {
 		String boardStr = "";
-		
+
 		for(int i=0;i<height;i++){
 			for(int j=0;j<width;j++){
 				boardStr += "" + board[i][j]+" ";
 			}
 			boardStr += "\n";
 		}
-		
+
 		return boardStr;
 	}
-
-	//Checks to see if the given player can validly remove a piece from bottom
-	//Check this function, seems weird
-	public boolean canRemoveADiscFromBottom(int col, int currentPlayer){
-		if(col<0 || col>=this.width) {
-			return false;
-		}
-		else if(board[height-1][col]!=currentPlayer){
-			return false;
-		}
-		else 
-			return true;
-	}
-
-	//Actually remove a disc from the bottom
-	//This currently just alters this board
-	public void removeADiscFromBottom(int col){
-		int i;
-		for(i=height-1;i>height-this.numOfDiscsInColumn[col];i--){
-			board[i][col]=board[i-1][col];
-		}
-		board[i][col]=this.emptyCell;
-		this.numOfDiscsInColumn[col]--;
-	}
-
-	//Checks to see if dropping move is valid
-	public boolean canDropADiscFromTop(int col, int currentPlayer){
-		if(col<0 || col>=this.width) {
-			return false;
-		}
-		else if(this.numOfDiscsInColumn[col]==this.height){
-			return false;
-		}
-		else
-			return true;
-	}
-
-	//Alters this board to Drop piece
-	public void dropADiscFromTop(int col, int currentplayer){
-		int firstEmptyCellRow=height-this.numOfDiscsInColumn[col]-1;
-		board[firstEmptyCellRow][col]=currentplayer;
-		this.numOfDiscsInColumn[col]++;
-	}
-
-	//Checks to see if board is already full
-	public boolean isFull(){
-		for(int i=0;i<height;i++)
-			for(int j=0;j<width;j++){
-				if(board[i][j]==this.emptyCell)
-					return false;
-			}
-		return true;
-	}
-
-	//Update the board with the given row, column and playerNumber
-	public void setBoard(int row, int col, int player){
-		if(row>=height || col>=width)
-			throw new IllegalArgumentException("The row or column number is out of bound!");
-		if(player!=this.PLAYER1 && player!=this.PLAYER2)
-			throw new IllegalArgumentException("Wrong player!");
-		this.board[row][col]=player;
-	}
-	
-	//Check to see if there is a winner
-	public int isConnectN(){
-		int tmp_winner=checkHorizontally();
-
-		if(tmp_winner!=this.NOCONNECTION)
-			return tmp_winner;
-
-		tmp_winner=checkVertically();
-		if(tmp_winner!=this.NOCONNECTION)
-			return tmp_winner;
-
-		tmp_winner=checkDiagonally1();
-		if(tmp_winner!=this.NOCONNECTION)
-			return tmp_winner; 
-		tmp_winner=checkDiagonally2();
-		if(tmp_winner!=this.NOCONNECTION)
-			return tmp_winner; 
-
-		return this.NOCONNECTION;
-	}
-
-	//Check the board horizontally to see if there is a winner
-	public int checkHorizontally(){
-		int max1=0;
-		int max2=0;
-		boolean player1_win=false;
-		boolean player2_win=false;
-		//check each row, horizontally
-		for(int i=0;i<this.height;i++){
-			max1=0;
-			max2=0;
-			for(int j=0;j<this.width;j++){
-				if(board[i][j]==PLAYER1){
-					max1++;
-					max2=0;
-					if(max1==N)
-						player1_win=true;
-				}
-				else if(board[i][j]==PLAYER2){
-					max1=0;
-					max2++;
-					if(max2==N)
-						player2_win=true;
-				}
-				else{
-					max1=0;
-					max2=0;
-				}
-			}
-		} 
-		if (player1_win && player2_win)
-			return this.TIE;
-		if (player1_win)
-			return this.PLAYER1;
-		if (player2_win)
-			return this.PLAYER2;
-
-		return this.NOCONNECTION;
-	}
-
-
-	//Check the board vertically to see if there is a winner
-	public int checkVertically(){
-		//check each column, vertically
-		int max1=0;
-		int max2=0;
-		boolean player1_win=false;
-		boolean player2_win=false;
-
-		for(int j=0;j<this.width;j++){
-			max1=0;
-			max2=0;
-			for(int i=0;i<this.height;i++){
-				if(board[i][j]==PLAYER1){
-					max1++;
-					max2=0;
-					if(max1==N)
-						player1_win=true;
-				}
-				else if(board[i][j]==PLAYER2){
-					max1=0;
-					max2++;
-					if(max2==N)
-						player2_win=true;
-				}
-				else{
-					max1=0;
-					max2=0;
-				}
-			}
-		} 
-		if (player1_win && player2_win)
-			return this.TIE;
-		if (player1_win)
-			return this.PLAYER1;
-		if (player2_win)
-			return this.PLAYER2;
-
-		return this.NOCONNECTION;
-	}
-
-	//Check the board diagonally to see if there is a winner
-	public int checkDiagonally1(){
-		//check diagonally y=-x+k
-		int max1=0;
-		int max2=0;
-		boolean player1_win=false;
-		boolean player2_win=false;
-		int upper_bound=height-1+width-1-(N-1);
-
-		for(int k=N-1;k<=upper_bound;k++){			
-			max1=0;
-			max2=0;
-			int x,y;
-			if(k<width) 
-				x=k;
-			else
-				x=width-1;
-			y=-x+k;
-
-			while(x>=0  && y<height){
-				// System.out.println("k: "+k+", x: "+x+", y: "+y);
-				if(board[height-1-y][x]==PLAYER1){
-					max1++;
-					max2=0;
-					if(max1==N)
-						player1_win=true;
-				}
-				else if(board[height-1-y][x]==PLAYER2){
-					max1=0;
-					max2++;
-					if(max2==N)
-						player2_win=true;
-				}
-				else{
-					max1=0;
-					max2=0;
-				}
-				x--;
-				y++;
-			}	 
-
-		}
-		if (player1_win && player2_win)
-			return this.TIE;
-		if (player1_win)
-			return this.PLAYER1;
-		if (player2_win)
-			return this.PLAYER2;
-
-		return this.NOCONNECTION;
-	}
-
-	//Check the board diagonally to see if there is a winner
-	public int checkDiagonally2(){
-		//check diagonally y=x-k
-		int max1=0;
-		int max2=0;
-		boolean player1_win=false;
-		boolean player2_win=false;
-		int upper_bound=width-1-(N-1);
-		int  lower_bound=-(height-1-(N-1));
-		// System.out.println("lower: "+lower_bound+", upper_bound: "+upper_bound);
-		for(int k=lower_bound;k<=upper_bound;k++){			
-			max1=0;
-			max2=0;
-			int x,y;
-			if(k>=0) 
-				x=k;
-			else
-				x=0;
-			y=x-k;
-			while(x>=0 && x<width && y<height){
-				// System.out.println("k: "+k+", x: "+x+", y: "+y);
-				if(board[height-1-y][x]==PLAYER1){
-					max1++;
-					max2=0;
-					if(max1==N)
-						player1_win=true;
-				}
-				else if(board[height-1-y][x]==PLAYER2){
-					max1=0;
-					max2++;
-					if(max2==N)
-						player2_win=true;
-				}
-				else{
-					max1=0;
-					max2=0;
-				}
-				x++;
-				y++;
-			}	 
-
-		}	 //end for y=x-k
-
-		if (player1_win && player2_win)
-			return this.TIE;
-		if (player1_win)
-			return this.PLAYER1;
-		if (player2_win)
-			return this.PLAYER2;
-
-		return this.NOCONNECTION;
-	}
-
-
 
 	/*
 	 * *********************************************************************************
@@ -484,15 +155,10 @@ public class Board {
 				if(board[i][j]==PLAYER1){
 					max1++;
 					max2=0;
-					
-					if(max1 == N)
-						return 1;
 				}
 				else if(board[i][j]==PLAYER2){
 					max1=0;
 					max2++;
-					if(max2 == N)
-						return -1;
 				}
 				else{
 					if(max1 >= numInARow) {
@@ -505,8 +171,21 @@ public class Board {
 					max2=0;
 				}
 			}
-		} 
-			return player1Count - player2Count;
+		}
+
+		if(DataReader.debugMode) {
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write(numInARow + "     H: P1 = " + player1Count + "\n");
+				fw.write(numInARow + "        P2 = " + player2Count + "\n");
+				fw.write(numInARow + "        Total = " + (player1Count - player2Count) + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return player1Count - player2Count;
 	}
 
 	//Used for the get heuristic function to evaluate a given board
@@ -524,14 +203,10 @@ public class Board {
 				if(board[i][j]==PLAYER1){
 					max1++;
 					max2=0;
-					if(max1 == N) 
-						return 1;
 				}
 				else if(board[i][j]==PLAYER2){
 					max1=0;
 					max2++;
-					if(max2 == N)
-						return -1;
 				}
 				else{
 					if(max1 >= numInARow) {
@@ -546,7 +221,19 @@ public class Board {
 			}
 		}
 
-			return player1Count - player2Count;
+		if(DataReader.debugMode) {
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write(numInARow + "    Hr: P1 = " + player1Count + "\n");
+				fw.write(numInARow + "        P2 = " + player2Count + "\n");
+				fw.write(numInARow + "        Total = " + (player1Count - player2Count) + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return player1Count - player2Count;
 	}
 
 	//The heuristic function uses this to determine if this is a good board state
@@ -564,14 +251,10 @@ public class Board {
 				if(board[i][j]==PLAYER1){
 					max1++;
 					max2=0;
-					if(max1==N)
-						return 1;
 				}
 				else if(board[i][j]==PLAYER2){
 					max1=0;
 					max2++;
-					if(max2==N)
-						return -1;
 				}
 				else{
 					if(max1 >= numInARow)
@@ -582,8 +265,66 @@ public class Board {
 					max2=0;
 				}
 			}
-		} 
-			return player1Count - player2Count;
+		}
+
+		if(DataReader.debugMode) {	
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write(numInARow + "     V: P1 = " + player1Count + "\n");
+				fw.write(numInARow + "        P2 = " + player2Count + "\n");
+				fw.write(numInARow + "        Total = " + (player1Count - player2Count) + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return player1Count - player2Count;
+	}
+
+	public int checkVReverse(int numInARow){
+		//check each column, vertically
+		int max1=0;
+		int max2=0;
+		int player1Count = 0;
+		int player2Count = 0;
+
+		for(int j=0;j<this.width;j++){
+			max1=0;
+			max2=0;
+			for(int i=this.height - 1;i>=0;i--){
+				if(board[i][j]==PLAYER1){
+					max1++;
+					max2=0;
+				}
+				else if(board[i][j]==PLAYER2){
+					max1=0;
+					max2++;
+				}
+				else{
+					if(max1 >= numInARow)
+						player1Count++;
+					if(max2 >= numInARow)
+						player2Count++;
+					max1=0;
+					max2=0;
+				}
+			}
+		}
+
+		if(DataReader.debugMode) {	
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write(numInARow + "    Vr: P1 = " + player1Count + "\n");
+				fw.write(numInARow + "        P2 = " + player2Count + "\n");
+				fw.write(numInARow + "        Total = " + (player1Count - player2Count) + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return player1Count - player2Count;
 	}
 
 	//Used by the heuristic function to determine how many diags in a row we have
@@ -610,14 +351,10 @@ public class Board {
 				if(board[height-1-y][x]==PLAYER1){
 					max1++;
 					max2=0;
-					if(max1 == N)
-						return 1;
 				}
 				else if(board[height-1-y][x]==PLAYER2){
 					max1=0;
 					max2++;
-					if(max2==N)
-						return -1;
 				}
 				else{
 					if(max1 >= numInARow) {
@@ -634,7 +371,19 @@ public class Board {
 			}	  
 		}
 
-			return player1Count - player2Count;
+		if(DataReader.debugMode) {
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write(numInARow + "    D1: P1 = " + player1Count + "\n");
+				fw.write(numInARow + "        P2 = " + player2Count + "\n");
+				fw.write(numInARow + "        Total = " + (player1Count - player2Count) + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return player1Count - player2Count;
 	}
 
 	//Check the board diagonally to see if there is a winner
@@ -661,14 +410,10 @@ public class Board {
 				if(board[height-1-y][x]==PLAYER1){
 					max1++;
 					max2=0;
-					if(max1==N)
-						return 1;
 				}
 				else if(board[height-1-y][x]==PLAYER2){
 					max1=0;
 					max2++;
-					if(max2==N)
-						return -1;
 				}
 				else{
 					if(max1 >= numInARow) 
@@ -681,9 +426,20 @@ public class Board {
 				x++;
 				y++;
 			}	 
+		}
 
-		}	 //end for y=x-k
+		if(DataReader.debugMode) {
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt", true);
+				fw.write(numInARow + "    D2: P1 = " + player1Count + "\n");
+				fw.write(numInARow + "        P2 = " + player2Count + "\n");
+				fw.write(numInARow + "        Total = " + (player1Count - player2Count) + "\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-			return player1Count - player2Count;
+		return player1Count - player2Count;
 	}
 }
--- src/featureGenerator/DataReader.java
+++ src/featureGenerator/DataReader.java
@@ -6,9 +6,11 @@ package featureGenerator;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * @author Dan True, Nick ***
@@ -18,10 +20,25 @@ public class DataReader {
 
 	private static String inFilePath;  // Training data (without Features)
 	private static String outFilePath; // Training data (with Features)
-	
+
 	private ArrayList<String> boardStrList;         // Board data from file (Strings)
 	private ArrayList<Board> boardList;             // Board data from file (Board objects)
 	
+	public static boolean debugMode = true;
+
+	public DataReader() {
+		if(debugMode) {
+			try {
+				FileWriter fw = new FileWriter("debugFile.txt");
+				Date d = new Date();
+				fw.write(d.toString() + "\n\n");
+				fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/*
 	 * Parses the training data file into a list of Strings.
 	 * Each String in the list is a different board.
@@ -31,14 +48,14 @@ public class DataReader {
 		boardStrList = new ArrayList<String>();
 		String boardStr;
 		BufferedReader br;
-		
+
 		try {
 			br = new BufferedReader(new FileReader(inFilePath));
-			
+
 			while((boardStr = br.readLine()) != null) {
 				boardStrList.add(boardStr);
 			}
-			
+
 			br.close();
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
@@ -46,50 +63,68 @@ public class DataReader {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
 	 * Parses the list of Strings into a list of Board objects.
 	 * Also, creates another list of boardStr's without the winner.
 	 */
 	public void processBoards() {
 		boardList = new ArrayList<Board>();
-		
-		for(String bStr: boardStrList) {
-			boardList.add(Board.createBoardFromString(bStr));
+		int count = 0;
+
+		try {
+
+			for(String bStr: boardStrList) {
+				boardList.add(Board.createBoardFromString(bStr));
+
+				if(debugMode) {
+					FileWriter fw = new FileWriter("debugFile.txt", true);
+					fw.write("------------------------------------\n");
+					fw.write("Board " + count + "\n\n");				
+					fw.write(boardList.get(count).printBoard() + "\n\n");
+					fw.close();
+				}
+
+				boardList.get(count).calculateFeatureValues();
+				count++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
+
+
 	/*
 	 * For each of the Boards in boardList, calculate their feature values.
 	 * Then output the original board data, but add the feature values.
 	 */
 	public void getFeatureValues() {
-		
+
 		try {
 			PrintWriter writer = new PrintWriter(outFilePath);
-			
+
 			writer.println("f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,"
 					+ "f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,"
 					+ "f21,f22,f23,f24,f25,f26,f27,f28,f29,f30,"
 					+ "f31,f32,f33,f34,f35,f36,f37,f38,f39,f40,"
 					+ "f41,f42,Middle,Connect2,Connect3,"
-					+ "empty4,empty5,Winner");
+					+ "Total,empty5,Winner");
+
 			for(int i = 0; i < boardList.size(); i++) {
 				String currentBStr = boardStrList.get(i);
 				String featureVals = boardList.get(i).calculateFeatureValues();
-				
+
 				String bStrWithoutWinner = currentBStr.substring(0, currentBStr.length() - 1);
 				writer.println(bStrWithoutWinner + featureVals + "," + currentBStr.charAt(currentBStr.length() - 1));
 			}
-			
+
 			writer.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
 	 * Main entry to program.
 	 * Takes in 2 arguments:
@@ -97,22 +132,22 @@ public class DataReader {
 	 *      name of output file for new data (data with feature values)
 	 */
 	public static void main(String[] args) {
-		
+
 		// Exit if there is an incorrect number of arguments
 		if(args.length != 2) {
 			System.out.println("Error: Wrong number of parameters.");
 			System.exit(1);
 		}
-		
+
 		inFilePath = args[0];
 		outFilePath = args[1];
-		
+
 		DataReader dReader = new DataReader();
-		
+
 		dReader.readInData();       // Read in data from file
 		dReader.processBoards();    // Parse data
 		dReader.getFeatureValues(); // Calculate feature values, and output file with new data
-		
+
 		System.exit(0);
 	}
 }
